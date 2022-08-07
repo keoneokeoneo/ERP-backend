6330fcbfd389d38ef7403eafeaacc5986b4ddeea
keoneokeoneo <kimganho12@gmail.com>
2022-08-07 18:58:03 +0900

    feat: connect database with typeorm

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const config: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts, .js}'],
+  charset: 'utf8mb4',
+  synchronize: false,
+  logging: true,
+};
+
+export = config;
